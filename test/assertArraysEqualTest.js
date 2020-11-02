bdf39e8964a5eb9bdfbd62ea0dc2eabab638561f
Pavel Piatetskii <pavel.piatetskii@gmail.com>
2020-11-02 21:02:53 +0000

    Continue DRYing with the assertArraysEqual function

---
+++ test/assertArraysEqualTest.js
@@ -0,0 +1,9 @@
+// FUNCTION IMPORT
+const assertArraysEqual = require('../assertArraysEqual');
+
+// TEST CODE
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 2, 3], [3, 2, 1]);
+
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
