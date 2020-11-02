d1894020acf3660aeba3957d8d9ee330527c82b7
Pavel Piatetskii <pavel.piatetskii@gmail.com>
2020-11-02 20:58:14 +0000

    Continue DRYing with the eqArrays function

--- eqArrays.js
+++ eqArrays.js
@@ -1,21 +1,14 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✔️  Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`⚠️  Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) return false;
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  
   for (let i in array1) {
-    if (array1[i] !== array2[i]) return false;
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
   }
   return true;
 };
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
-
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]),true); // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
+module.exports = eqArrays;
