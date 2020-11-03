613b82461d6ebf6be72d37f0eb916f4bc7a9fae5
Pavel Piatetskii <pavel.piatetskii@gmail.com>
2020-11-03 04:48:28 +0000

    Made eqArrays able to work with multi-layer arrays (recursion)

--- eqArrays.js
+++ eqArrays.js
@@ -1,10 +1,16 @@
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
+const eqArrays = function(arr1, arr2) {
+
+  if (arr1.length !== arr2.length ||
+    !Array.isArray(arr1) || !Array.isArray(arr2)) {
     return false;
   }
   
-  for (let i in array1) {
-    if (array1[i] !== array2[i]) {
+  for (let i in arr1) {
+    if (Array.isArray(arr1[i]) && Array.isArray(arr2[i])) {
+      if (!eqArrays(arr1[i], arr2[i])) {
+        return false;
+      }
+    } else if (arr1[i] !== arr2[i]) {
       return false;
     }
   }
@@ -12,3 +18,8 @@ const eqArrays = function(array1, array2) {
 };
 
 module.exports = eqArrays;
+
+//console.log(eqArrays([[2, 3], [4]], [[2, 3], [4]])) // => true
+//
+//console.log(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]])) // => false
+//console.log(eqArrays([[2, 3], [4]], [[2, 3], 4])) // => false
